bf025fd405f67f8b9f6cc445926628adb17abe67
monge1h <jlo4321jl@gmail.com>
2020-01-20 22:01:16 -0600

    Add my custom filter function

--- overrideMethods/arrayMethods.js
+++ overrideMethods/arrayMethods.js
@@ -1,11 +1,28 @@
-const myCustomMapFunction = function (cb){
+let test = [1,2,3,4,5,6]
+
+const myCustomMapFunction = function(cb){
     const newArray = []
     for(let i = 0; i<this.length; i++){
-        newArray[i] = cb(this[i],i)
+        newArray[i] = cb(this[i])
     }
     return newArray
 }
 Array.prototype.map = myCustomMapFunction
 
-let test = [1,2,3]
-let result = test.map(e=>e * 2)
+test.map(e=>e * 2)
+// output -- [ 2, 4, 6, 8, 10, 12 ]
+
+const myCustomFilterFunction = function(cb){
+    const newArray = []
+    let j = 0
+    for(let i = 0; i<this.length; i++){
+        if(cb(this[i])){
+            newArray[j++] = this[i]
+        }
+    }
+    return newArray
+}
+
+Array.prototype.filter = myCustomFilterFunction
+test.filter(x=>x%2==0)
+// output -- [2,4,6]
